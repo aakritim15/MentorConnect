12bc843425049f9ca302c63b556ed60e96c4c4f6
Nilanshjain <nilanshjain0306@gmail.com>
2024-09-05 17:34:01 +0530

    dashboard

---
+++ mentorconnect/frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,271 @@
+import './Dashboard.css';
+
+
+function Dashboard(){
+  	return (
+    		<div className="dashboard">
+      			<div className="frame-parent">
+        				<div className="sidebar-link-wrapper">
+          					<div className="sidebar-link">
+            						<img className="iconsearch" alt="" src="Icon/Search.svg" />
+            						<div className="search">Search</div>
+          					</div>
+        				</div>
+        				<div className="frame-group">
+          					<img className="frame-icon" alt="" src="Frame.svg" />
+          					<img className="frame-icon" alt="" src="Frame.svg" />
+        				</div>
+        				<img className="frame-child" alt="" src="Group 5.svg" />
+      			</div>
+      			<div className="sidebar">
+        				<div className="div">2</div>
+        				<div className="group-parent">
+          					<div className="sidebar-link-parent">
+            						<div className="sidebar-link1">
+              							<img className="icondashboard" alt="" src="Icon/Dashboard.svg" />
+              							<div className="dahboard">Dahboard</div>
+            						</div>
+            						<div className="sidebar-link2">
+              							<img className="icondashboard" alt="" src="Icon/Pow.svg" />
+              							<div className="dahboard">Activity</div>
+            						</div>
+            						<div className="menu">MENU</div>
+          					</div>
+          					<img className="frame-item" alt="" src="Vector 6.svg" />
+          					<img className="frame-item" alt="" src="Vector 5.svg" />
+          					<div className="sidebar-link-parent">
+            						<div className="sidebar-link3">
+              							<div className="iconchat-parent">
+                								<img className="icondashboard" alt="" src="Icon/Chat.svg" />
+                								<div className="dahboard">Chat</div>
+              							</div>
+              							<div className="rectangle-parent">
+                								<img className="group-child" alt="" src="Rectangle 3.png" />
+                								<img className="group-item" alt="" src="Rectangle 1.png" />
+                								<img className="group-inner" alt="" src="Rectangle 2.png" />
+              							</div>
+            						</div>
+            						<div className="sidebar-link4">
+              							<div className="iconcalendar-parent">
+                								<img className="frame-icon" alt="" src="Icon/Calendar.svg" />
+                								<div className="search">Appointments</div>
+              							</div>
+              							<div className="sidebar-link5">
+                								<div className="dahboard">2</div>
+              							</div>
+            						</div>
+            						<div className="menu">SCHEDULE</div>
+          					</div>
+          					<img className="frame-item" alt="" src="Vector 4.svg" />
+          					<div className="sidebar-link-parent">
+            						<div className="menu">HELP</div>
+            						<div className="sidebar-link-container">
+              							<div className="sidebar-link6">
+                								<img className="icondashboard" alt="" src="material-symbols:star-rate.svg" />
+                								<div className="dahboard">Ratings</div>
+              							</div>
+              							<div className="sidebar-link6">
+                								<img className="icondashboard" alt="" src="Icon/Documentation.svg" />
+                								<div className="dahboard">FAQ</div>
+              							</div>
+            						</div>
+          					</div>
+        				</div>
+        				<div className="sidebar-link8">
+          					<img className="icondashboard" alt="" src="Frame.svg" />
+          					<div className="dahboard">Log out</div>
+        				</div>
+      			</div>
+      			<div className="dashboard-wrapper">
+        				<b className="career-catalyst">Dashboard</b>
+      			</div>
+      			<div className="welcome-back-richard-d-wrapper">
+        				<div className="welcome-back-richard">Welcome back, Richard D</div>
+      			</div>
+      			<div className="rectangle-group">
+        				<div className="rectangle-div" />
+        				<img className="majesticonsopen" alt="" src="majesticons:open.svg" />
+        				<div className="frame-div" />
+        				<div className="rectangle" />
+      			</div>
+      			<div className="rectangle-container">
+        				<div className="rectangle-div" />
+        				<img className="majesticonsopen" alt="" src="majesticons:open.svg" />
+        				<div className="frame-div" />
+        				<div className="rectangle" />
+      			</div>
+      			<div className="group-div">
+        				<div className="rectangle-div" />
+        				<img className="majesticonsopen" alt="" src="majesticons:open.svg" />
+        				<div className="frame-div" />
+        				<div className="rectangle" />
+      			</div>
+      			<div className="rectangle-parent1">
+        				<div className="rectangle-div" />
+        				<img className="majesticonsopen" alt="" src="majesticons:open.svg" />
+        				<div className="frame-div" />
+        				<div className="rectangle" />
+      			</div>
+      			<div className="rectangle-parent2">
+        				<div className="rectangle-div" />
+        				<img className="majesticonsopen" alt="" src="majesticons:open.svg" />
+        				<div className="frame-div" />
+        				<div className="rectangle" />
+      			</div>
+      			<div className="rectangle-parent3">
+        				<div className="rectangle-div" />
+        				<img className="majesticonsopen" alt="" src="majesticons:open.svg" />
+        				<div className="frame-div" />
+        				<div className="rectangle" />
+      			</div>
+      			<div className="rectangle-parent4">
+        				<div className="rectangle-div" />
+        				<img className="majesticonsopen" alt="" src="majesticons:open.svg" />
+        				<div className="frame-div" />
+        				<div className="rectangle" />
+      			</div>
+      			<div className="rectangle-parent5">
+        				<div className="rectangle-div" />
+        				<img className="majesticonsopen" alt="" src="majesticons:open.svg" />
+        				<div className="frame-div" />
+        				<div className="rectangle" />
+      			</div>
+      			<div className="rectangle-parent6">
+        				<div className="rectangle-div" />
+        				<img className="majesticonsopen" alt="" src="majesticons:open.svg" />
+        				<div className="frame-div" />
+        				<div className="rectangle" />
+      			</div>
+      			<div className="rectangle-parent7">
+        				<div className="rectangle-div" />
+        				<img className="majesticonsopen" alt="" src="majesticons:open.svg" />
+        				<div className="frame-div" />
+        				<div className="rectangle" />
+      			</div>
+      			<div className="rectangle-parent8">
+        				<div className="rectangle-div" />
+        				<img className="majesticonsopen" alt="" src="majesticons:open.svg" />
+        				<div className="frame-div" />
+        				<div className="rectangle" />
+      			</div>
+      			<div className="rectangle-parent9">
+        				<div className="rectangle-div" />
+        				<img className="majesticonsopen" alt="" src="majesticons:open.svg" />
+        				<div className="frame-div" />
+        				<div className="rectangle" />
+      			</div>
+      			<div className="scrollbar-custom">
+        				<div className="bar" />
+      			</div>
+      			<div className="career-catalyst-wrapper">
+        				<b className="career-catalyst">Career Catalyst</b>
+      			</div>
+      			<div className="futurelead-wrapper">
+        				<b className="career-catalyst">FutureLead</b>
+      			</div>
+      			<div className="pathpro-wrapper">
+        				<b className="career-catalyst">PathPro</b>
+      			</div>
+      			<div className="startmentor-wrapper">
+        				<b className="career-catalyst">StartMentor</b>
+      			</div>
+      			<div className="skillmastery-wrapper">
+        				<b className="career-catalyst">SkillMastery</b>
+      			</div>
+      			<div className="careerfix-wrapper">
+        				<b className="career-catalyst">CareerFix</b>
+      			</div>
+      			<div className="mindsetshift-wrapper">
+        				<b className="career-catalyst">MindsetShift</b>
+      			</div>
+      			<div className="innovmentor-wrapper">
+        				<b className="career-catalyst">InnovMentor</b>
+      			</div>
+      			<div className="creative-mentor-wrapper">
+        				<b className="career-catalyst">Creative Mentor</b>
+      			</div>
+      			<div className="learnpro-wrapper">
+        				<b className="career-catalyst">LearnPro</b>
+      			</div>
+      			<div className="skillsprint-wrapper">
+        				<b className="career-catalyst">SkillSprint</b>
+      			</div>
+      			<div className="pivotup-wrapper">
+        				<b className="career-catalyst">PivotUp</b>
+      			</div>
+      			<div className="unlock-your-potential-with-exp-wrapper">
+        				<div className="unlock-your-potential">Unlock your potential with expert guidance and personalized career strate..</div>
+      			</div>
+      			<div className="step-into-leadership-with-expe-wrapper">
+        				<div className="unlock-your-potential">Step into leadership with expert guidance and actionable advice.</div>
+      			</div>
+      			<div className="chart-your-career-path-with-ex-wrapper">
+        				<div className="unlock-your-potential">Chart your career path with expert mentors by your side.</div>
+      			</div>
+      			<div className="kickstart-your-journey-with-ta-wrapper">
+        				<div className="unlock-your-potential">Kickstart your journey with tailored advice from seasoned mentors.</div>
+      			</div>
+      			<div className="learn-essential-industry-skill-wrapper">
+        				<div className="unlock-your-potential">Learn essential industry skills from top mentors and advance your professional..</div>
+      			</div>
+      			<div className="get-personalized-mentorship-to-wrapper">
+        				<div className="unlock-your-potential">Get personalized mentorship to fix career roadblocks and boost your progress.</div>
+      			</div>
+      			<div className="transform-your-mindset-for-suc-wrapper">
+        				<div className="unlock-your-potential">Transform your mindset for success with practical mentorship.</div>
+      			</div>
+      			<div className="innovate-smarter-with-personal-wrapper">
+        				<div className="unlock-your-potential">Innovate smarter with personalized support from industry leaders.</div>
+      			</div>
+      			<div className="hone-your-creative-talents-wit-wrapper">
+        				<div className="unlock-your-potential">Hone your creative talents with insights and guidance from experienced professi..</div>
+      			</div>
+      			<div className="achieve-mastery-in-your-field-wrapper">
+        				<div className="unlock-your-potential">Achieve mastery in your field through mentor-driven learning.</div>
+      			</div>
+      			<div className="learn-and-grow-quickly-with-fa-wrapper">
+        				<div className="unlock-your-potential">Learn and grow quickly with fast-paced, focused mentoring.</div>
+      			</div>
+      			<div className="make-a-successful-career-pivot-wrapper">
+        				<div className="unlock-your-potential">Make a successful career pivot with experienced mentor guidance.</div>
+      			</div>
+      			<div className="dashboard-child" />
+      			<div className="dashboard-item" />
+      			<div className="dashboard-inner" />
+      			<div className="dashboard-child1" />
+      			<div className="dashboard-child2" />
+      			<div className="dashboard-child3" />
+      			<div className="dashboard-child4" />
+      			<div className="dashboard-child5" />
+      			<div className="dashboard-child6" />
+      			<div className="dashboard-child7" />
+      			<div className="dashboard-child8" />
+      			<div className="dashboard-child9" />
+      			<div className="buy-now">Buy Now</div>
+      			<div className="buy-now1">Buy Now</div>
+      			<div className="buy-now2">Buy Now</div>
+      			<div className="buy-now3">Buy Now</div>
+      			<div className="buy-now4">Buy Now</div>
+      			<div className="buy-now5">Buy Now</div>
+      			<div className="buy-now6">Buy Now</div>
+      			<div className="buy-now7">Buy Now</div>
+      			<div className="buy-now8">Buy Now</div>
+      			<div className="buy-now9">Buy Now</div>
+      			<div className="buy-now10">Buy Now</div>
+      			<div className="buy-now11">Buy Now</div>
+      			<img className="rectangle-icon" alt="" src="Rectangle 393.png" />
+      			<img className="dashboard-child10" alt="" src="Rectangle 410.png" />
+      			<img className="dashboard-child11" alt="" src="Rectangle 401.png" />
+      			<img className="dashboard-child12" alt="" src="Rectangle 411.png" />
+      			<img className="dashboard-child13" alt="" src="Rectangle 395.png" />
+      			<img className="dashboard-child14" alt="" src="Rectangle 412.png" />
+      			<img className="dashboard-child15" alt="" src="Rectangle 402.png" />
+      			<img className="dashboard-child16" alt="" src="Rectangle 413.png" />
+      			<img className="dashboard-child17" alt="" src="Rectangle 397.png" />
+      			<img className="dashboard-child18" alt="" src="Rectangle 414.png" />
+      			<img className="dashboard-child19" alt="" src="Rectangle 403.png" />
+      			<img className="dashboard-child20" alt="" src="Rectangle 415.png" />
+    		</div>);
+};
+
+export default Dashboard;
